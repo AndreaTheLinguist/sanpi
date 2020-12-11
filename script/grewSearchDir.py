eec4192b7695144fb727cbca431cecc047505fba
Andrea <andrea.r.hummel@gmail.com>
2020-12-11 12:32:20 -0500

    added option to skip previously searched files

--- script/grewSearchDir.py
+++ script/grewSearchDir.py
@@ -24,6 +24,14 @@ def __main__():
 
         filePref, _ = file.split('.')
         filePathName = f'{dirPath}/{file}'
+        outputFilename = f'{filePref}.raw.json'
+
+        # do not run grew search if output (raw) json file already exists
+        if args.skip_files and outputFilename in os.listdir(outputDir):
+            print(
+                f'Output file {outputFilename} already exists for {filePathName}. Grew search will not be re-run.')
+            continue
+
         grew_cmd_str = f'grew grep -pattern {patPath} -i {filePathName} > {outputDir}/{filePref}.raw.json'
         print(f'searching {filePathName}...')
         print('grew command: ', grew_cmd_str)
@@ -59,11 +67,15 @@ def parseArgs():
     parser.add_argument('dir', type=Path, default=Path.cwd(),
                         help='path to directory containing conll files to search for grew pattern')
 
-    parser.add_argument(
-        'pat', type=Path, help='path to grew pattern to search for')
+    parser.add_argument('pat', type=Path,
+                        help='path to grew pattern to search for')
 
-    parser.add_argument(
-        'output', type=str, help='output directory name for. Should correspond to sentence data source and pattern like so: "<data set>.<pattern name>", e.g. "Nyt1.p1"')
+    parser.add_argument('output', type=str,
+                        help='output directory name for. Should correspond to sentence data source and pattern like so: "<data set>.<pattern name>", e.g. "Nyt1.p1"')
+
+    parser.add_argument('-s', '--skip_files', action='store_true',
+                        default=False,
+                        help='option to skip corpus files that already have a corresponding output in the given json directory. Grew search will not be rerun and pre-existing raw json file will be preserved.')
 
     return parser.parse_args()
 
