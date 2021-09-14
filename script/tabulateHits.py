2d132b00162ed7bb953be3ae81046f55dc88bcb1
Andrea <andrea.r.hummel@gmail.com>
2021-09-13 23:03:50 -0400

    added processing for `neg` and `relay` dependency labels

--- script/tabulateHits.py
+++ script/tabulateHits.py
@@ -9,10 +9,6 @@ from pathlib import Path
 
 import pandas as pd
 
-# hit_tuple = namedtuple(
-    # 'hit_tuple', ['hit_id', 'adv', 'adj', 'sent_text', 'sent_id', 'adv_index',
-    #               'json_source', 'prev_sent', 'next_sent', 'all_tags', 'all_edges'])
-
 
 def __main__():
 
@@ -29,10 +25,12 @@ def __main__():
 
     hit_count = len(hit_data)
     if hit_count > 0:
-        print(f'\n^_^ Finished collecting data for {hit_count}' 
-              'sentence(s) from all json files.\n'
+        print(f'\n^_^ Finished tabulating data for {hit_count}' 
+              f' sentence(s) from all json files in {json_dir}.\n'
               '-> Writing tables to csv files...')
         createOutput(hit_data, args)
+    else: 
+        sys.exit('No valid hits found.')
 
 
 def parseArgs():
@@ -80,8 +78,7 @@ def parseArgs():
 
 
 def getHitData(json_dir, args):
-
-    # pull out sentence info and create dictionary of hit_id : namedTuple
+    
     fileCount = 0
     df_from_json = pd.DataFrame()
 
@@ -115,7 +112,7 @@ def getHitData(json_dir, args):
             
             deps = get_deps(json_dict)
             tokens = get_tokens(json_dict)
-            match_ix = get_adv_ids(json_dict)
+            match_ix = get_match_ix(json_dict)
 
             match_df = tokens.join(deps).assign(match_ix=match_ix)
 
@@ -128,58 +125,7 @@ def getHitData(json_dir, args):
             hit_id = recombined_df.sent_id + ':' + recombined_df.match_ix
             recombined_df = recombined_df.assign(hit_id=hit_id)
             
-            df_from_json = pd.concat((df_from_json, recombined_df))
-            
-            # for hit in json_dict:
-
-                # sent_id = hit['sent_id']
-
-                # try:
-                #     fillers = hit['matching']['fillers']
-                # except KeyError:
-                #     print(f'-> Warning: Word info missing for {sent_id}. '
-                #           f'Skipping hit.')
-                #     continue
-
-                # try:
-                #     deps = hit['matching']['deps']
-                # except KeyError:
-                #     deps = {}
-                #     continue
-
-                # try:
-                #     # Adverb -> args.node1 = 'adv' by default
-                #     node1_word = fillers[args.node1].lower()
-                # except KeyError:
-                #     print(f'json entry missing first (ADV) node for '
-                #           f'hit {sent_id}. Skipping hit.')
-                #     continue
-
-                # try:
-                #     # Adjective -> args.node2 = 'adj' by default
-                #     node2_word = fillers[args.node2].lower()
-                # except KeyError:
-                #     print(f'json entry missing second (ADJ) node for '
-                #           f'hit {sent_id}. Skipping hit.')
-                #     continue
-
-                # sent_text = hit['text']
-                # prev_sent = hit['prev_sent']
-                # next_sent = hit['next_sent']
-
-                # # ADV index
-                # node1_index = hit['matching']['nodes'][args.node1]
-
-                # # hit_id = [sentID]:[node index in sentence of adv token]
-                # hit_id = ':'.join((sent_id, node1_index))
-
-                # hit_info = hit_tuple(
-                #     hit_id, node1_word, node2_word, sent_text, sent_id,
-                #     node1_index, jf.stem, prev_sent, next_sent, fillers, deps)
-
-                # hits_dict[hit_id] = hit_info
-
-    # return hits_dict     
+            df_from_json = pd.concat((df_from_json, recombined_df)) 
            
     return df_from_json.rename(columns={'doc': 'doc_id', 'text': 'sent_text'})
 
@@ -193,6 +139,7 @@ def get_deps(json_dict: dict):
         for dep_name, dep_info in hit.items(): 
             if type(dep_info[2]) == dict: 
                 dep_info[2] = dep_info[2]['1']
+                
 
             dep_name = dep_name+'_dep'
             hit_clean[dep_name] = dep_info
@@ -200,6 +147,17 @@ def get_deps(json_dict: dict):
         deps_clean.append(hit_clean)
 
     deps = pd.DataFrame(deps_clean)
+    # TODO : Add variables here as new dependency relation tags 
+    #   are introduced to/finalized in patterns
+    neg_label = relay_label = 'n/a'
+    if 'neg_dep' in deps.columns:
+        neg_label = [dep_list[2] for dep_list in deps.neg_dep]
+    if 'relay_dep' in deps.columns:
+        relay_label = [dep_list[2] for dep_list in deps.relay_dep]   
+        
+    deps = deps.assign(neg_deplabel = neg_label, 
+                       relay_deplabel = relay_label)
+
     return deps
      
      
@@ -218,7 +176,7 @@ def get_tokens(json_dict: dict):
     return tokens    
        
 
-def get_adv_ids(json_dict: dict):
+def get_match_ix(json_dict: dict):
     
     nodes = pd.DataFrame([hit['matching']['nodes']
                           for hit in json_dict])
@@ -241,15 +199,6 @@ def createOutput(hits_df, args):
     suffix = '.txt' if txt else '.csv'
 
     fname =  f'{args.outputPrefix}_hits{suffix}'
-
-    # hits_df = pd.DataFrame.from_dict(
-        # {k: v._asdict() for k, v in hits.items()}, orient='index')
-        
-    # if write_duplicates:
-        # hits_df = hits_df.assign(label=hits_df.index)
-        # hits_df = hits_df.assign(
-        #     status=hits_df.label.str.rsplit('_', 1).str.get(1))
-        # hits_df = hits_df.assign(status=hits_df.status.astype('category'))
    
     hits_df = hits_df.set_index('hit_id')
 
