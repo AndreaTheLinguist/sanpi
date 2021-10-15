5abfe8dc22bb917c2df016f186077518b2c80cff
Andrea <andrea.r.hummel@gmail.com>
2021-10-15 17:58:52 -0400

    updated `simple_view_hits.py` to work with new hits table columns. now takes in new arguments as well. Run with -h flag for more info

--- script/simple_view_hits.py
+++ script/simple_view_hits.py
@@ -1,33 +1,83 @@
-"""saves a simplified version (select columns only) of the hits table 
-    to a csv file with name '[label]_simplified.csv' to the parent directory of the original file. 
+"""saves a simplified version (select columns only) of the hits table
+    to a csv file with name '[label]_simplified.csv' to the parent directory of the original file.
 
     Arguments are: [csv path], [name of pat version (for labeling)]
     """
 import pandas as pd
-from sys import argv
+import argparse
+# from sys import argv
 from pathlib import Path
 
-# arguments are: [csv path], [name of pat version]
-source = Path(argv[1])
-label = argv[2]
+default_col_endings = ['colloc', 'window', 'word', 'id']
 
 
-def simplify(source, label):
+def main():
+    source, select_cols, label = parse_arg_input()
+    simplify(source, select_cols, label)
 
-    df = pd.read_csv(source)
-    word_info = [c for c in df.columns
-                 if c.endswith(('dep', 'word', 'deplabel'))]
-    added_col = [c for c in df.columns
-                 if c in ['sent_id', 'sent_text', 'window_text', 'colloc']]
-    df = df[word_info + added_col]
-    df = df.assign(pat_version=label).sort_values(
-        ['sent_text', 'sent_id', 'colloc'])
 
-    df = df[~df.duplicated(word_info.append('sent_text'))]
+def parse_arg_input():
+
+    parser = argparse.ArgumentParser(
+        description=('saves a simplified version(select columns only) of the hits '
+                     'table to a csv file with name `[label]_simplified.csv` '
+                     'to the parent directory of the original file.'))
+
+    parser.add_argument('full_hits', type=Path,
+                        help='path to full hits .csv to simplify.')
+
+    # TODO : give this default value corresponding to input file name
+    parser.add_argument('output_label', type=str,
+                        help='string for simplified file output file name')
+
+    parser.add_argument('-k', '--keep_col', action='append', dest='select_cols',
+                        metavar='COL_ENDING',
+                        help=('option to specify columns for simplified output by ending.'
+                              'Enter a string to match or partially match the column name: '
+                              '`-k word` will select all columns ending in "word"= adv_word, '
+                              'adj_word, neg_word, etc.; `-k adv_word` will keep columns ending '
+                              'in "adv_word" (only that exact column). As many string matches '
+                              'may be specified as desired (don\'t worry about possible overlap).'
+                              'If this flag is not used, script will use the following by default: '
+                              '{}'.format(default_col_endings)))
+
+    args = parser.parse_args()
+
+    select_cols = args.select_cols
+    if not select_cols:
+        select_cols = default_col_endings
+
+    return(args.full_hits, select_cols, args.output_label)
+
+
+def simplify(source, select_cols, label):
+
+    df = pd.read_csv(source).convert_dtypes()
+    all_cols = df.columns
+
+    selection = []
+    for substr in select_cols:
+        col_matches = [c for c in all_cols if c.endswith(substr)]
+        print(f'+ keeping ...{substr}: {col_matches}')
+        selection.extend(col_matches)
+
+    df = df[selection]
+
+    sort_cols = [i for i in ['sent_text', 'sent_id', 'colloc']
+                 if i in selection]
+    if sort_cols:
+        df = df.sort_values(sort_cols)
+        print('Output filtered for duplicates and sorted by:', sort_cols)
+        df = df[~df.duplicated(sort_cols)]
+    else:
+        print('Sorting columns not found:\n'
+              '-> output will not be sorted\n'
+              '-> duplicate filtering is not column dependent')
+        df = df[~df.duplicated()]
 
     pd.set_option('display.max_colwidth', 80)
 
-    sample = df[['colloc', 'sent_text']].sample(min(5, len(df)))
+    sample = df[sort_cols].sample(min(5, len(df)))
     print(sample)
 
     outpath = source.parent.joinpath(f'{label}_simplified.csv')
@@ -35,4 +85,6 @@ def simplify(source, label):
     print('...\n-> simplified viewing csv saved to', outpath)
 
 
-simplify(source, label)
+if __name__ == '__main__':
+
+    main()
