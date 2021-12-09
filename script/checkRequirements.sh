4e1698309604eecaa410a79223630d00b7427afd
Andrea <andrea.r.hummel@gmail.com>
2021-12-08 20:42:33 -0500

    added version check for `grew` to `script/checkRequirements.sh` script

--- script/checkRequirements.sh
+++ script/checkRequirements.sh
@@ -27,28 +27,40 @@ then
   if [[ $(echo "`which opam`" ) == "" ]]
   then
     echo "apt-get install opam"
-    apt-get install opam
+    # apt-get install opam
     echo "apt-get install wget m4 unzip librsvg2-bin curl bubblewrap"
-    apt-get install wget m4 unzip librsvg2-bin curl bubblewrap
+    # apt-get install wget m4 unzip librsvg2-bin curl bubblewrap
     echo "opam init"
-    opam init
+    # opam init
     echo "eval opam env"
-    eval opam env
+    # eval opam env
   fi
 
-  if [[ $(echo "`ocamlc -v`") < 4.1 ]]
+  if [[ $(echo "`ocamlc -v | cut -d " " -f 5 | head -1`") < 4.1 ]]
   then
-    echo "opam switch create 4.11.1"
-    opam switch create 4.11.1
-    echo "eval opam env"
-    eval opam env
+    echo "opam switch create 4.13.1"
+    # opam switch create 4.13.1
+    echo "eval $(opam env --switch=4.13.1)"
+    # eval $(opam env --switch=4.13.1) 
   fi
 
   echo "opam remote add grew \"http://opam.grew.fr\""
-  opam remote add grew "http://opam.grew.fr"
-  opam repository add grew --all-switches --set-default
+  # opam remote add grew "http://opam.grew.fr"
+  # opam repository add grew --all-switches --set-default
   echo "opam install grew grewpy"
-  opam install grew grewpy
+  # opam install grew grewpy
 fi
-echo "grew located in $(echo "`which grew`")"
+
+if [[ $(echo "`grew version | cut -d " " -f 2 | head -1`") < 1.7 ]]
+then
+  echo "grew version is out of date. Upgrading..."
+  echo "opam update"
+  # opam update
+  echo "opam upgrade"
+  # opam upgrade
+  echo "pip3 install grew --upgrade"
+  # pip3 install grew --upgrade
+fi
+
+echo "$(echo "`grew version | tail -1`"), located in $(echo "`which grew`")"
 echo "\`\`\`"
