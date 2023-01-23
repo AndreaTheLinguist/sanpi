c3f3dfc3e12363287bedeeb29dcc8649cc203867
Andrea Hummel <andrea.r.hummel@gmail.com>
2023-01-23 15:57:25 -0500

    corrected number of expected line breaks

--- script/adjust_conllu_blanklines.py
+++ script/adjust_conllu_blanklines.py
@@ -5,7 +5,7 @@ import time
 #// tmp = Path('/share/compling/projects/sanpi/playground/adjusted_conllus/tmp.txt')
 #// if not tmp.parent.is_dir():
 #//     tmp.parent.mkdir(parents=True)
-
+NEW_LINE_REQ = 2
 check_dir = Path(argv[1]).resolve()
 log_path = check_dir.joinpath(
     f'eof-blanks-adjust_{time.strftime("%Y-%m-%d_%R")}.log')
@@ -15,18 +15,18 @@ print(f'Adjusting blank lines at end of conllu files in {check_dir}...')
 for p in Path(check_dir).rglob('*conllu'):
     log_line = ''
     nlcount = p.read_text()[-15:].count('\n')
-    if nlcount > 3 or nlcount < 3:
+    if nlcount > NEW_LINE_REQ or nlcount < NEW_LINE_REQ:
         text = p.read_text(encoding='utf8')
 
-        while text[-15:].count('\n') > 3:
+        while text[-15:].count('\n') > NEW_LINE_REQ:
             #    text.endswith('\n'*4)
             text = text[:-1]
 
         # while text.endswith(('\n'*2, '\n'*1)) or not text.endswith('\n'):
-        while text[-15:].count('\n') < 3:
+        while text[-15:].count('\n') < NEW_LINE_REQ:
             text += '\n'
 
-        if text.endswith('\n'*3) and not text.endswith('\n'*4):
+        if text.endswith('\n'*NEW_LINE_REQ) and not text.endswith('\n'*(NEW_LINE_REQ + 1)):
             #// tmp.with_name(p.name).write_text(text, encoding='utf8')
             p.write_text(text, encoding='utf8')
             log_line = f'{p.stem} ✔'
