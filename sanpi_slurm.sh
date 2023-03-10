c2a05a31bf395727c68d6cc6d8c5278b724be39e
Andrea Hummel <andrea.r.hummel@gmail.com>
2023-03-10 18:00:43 -0500

    update to full corpora array slurm script for pipeline

--- sanpi_slurm.sh
+++ sanpi_slurm.sh
@@ -5,28 +5,28 @@
 #SBATCH -o %x_%2a.out                # Name of stdout output log file (%j expands to jobID)
 #SBATCH -e %x_%2a.err                # Name of stderr output log file (%j expands to jobID)
 #SBATCH --open-mode=append
+#SBATCH --array 0-31
 #SBATCH -N 1                            # Total number of nodes requested
 #SBATCH -n 1                            # Total number of cores requested
 #SBATCH --mem=11G                     # Total amount of (real) memory requested (per node)
 #SBATCH --time 12:00:00                  # Time limit (hh:mm:ss)
 #SBATCH --get-user-env
-#SBATCH --array 0-31
+#SBATCH --chdir=/share/compling/projects/sanpi/logs # to allow running `sbatch` cmd from anywhere and still put .out and .err in ../logs/ dir
 
 set -o errexit
 echo ">>=======================================<<"
 echo "JOB ID: ${SLURM_ARRAY_JOB_ID}"
 echo "started @ $(date) from $(pwd)"
+echo "slurm script: /share/compling/projects/sanpi/sanpi_slurm.sh"
 echo ""
 # activate conda environment
 eval "$(conda shell.bash hook)"
-conda activate sanpi
-echo "Active Environment:"
-echo "$(conda env list)"
-echo ""
+conda activate parallel-sanpi
+conda info | head -2 | tail -1
 
-# pattern directory should be specified as path argument
-PAT_DIR=$1
-echo "Pattern Type: ${PAT_DIR##*/}"
+# pattern directory should be specified as directory name
+PAT_DIR_NAME=${1:-"contig"}
+echo "Pattern Type: ${PAT_DIR_NAME##*/}"
 DATA_DIR=/share/compling/data
 if [[ ! -d ${DATA_DIR} ]]; then
     DATA_DIR=/home/arh234/data
@@ -40,8 +40,8 @@ else
     echo "Output will be saved to ${OUT_DIR}"
 fi
 
-LOGS_DIR=${OUT_DIR}/logs
-if [ ! -d "${LOGS_DIR}" ]; then
+LOGS_DIR="${OUT_DIR}/logs"
+if [[ ! -d "${LOGS_DIR}" ]]; then
     mkdir ${LOGS_DIR}
 fi
 
@@ -51,41 +51,65 @@ echo "   - partition: $SLURM_JOB_PARTITION"
 echo "   - node: $SLURM_JOB_NODELIST"
 echo "   - 1 of $SLURM_ARRAY_TASK_COUNT"
 
-SOURCE_DIR=/home/arh234/projects/sanpi
-# // PAT_DIR=${SOURCE_DIR}/Pat/${PAT}
+SOURCE_DIR=/share/compling/projects/sanpi
+PAT_DIR=${SOURCE_DIR}/Pat/${PAT_DIR_NAME}
 
 SEED=$((SLURM_ARRAY_TASK_ID))
 echo "Array Index = ${SEED}"
 
-if [ $SEED == 30 ]; then
-    echo "  Task ID ${SEED} assigned to 'test' dataset"
-    SEED="Te"
+## * Assign Array Job Inputs * ##
+# * news
+#> APW
+if [ $SEED == 32 ]; then
+    # echo "  Task ID ${SEED} assigned to 'Apw' dataset"
+    SEED_CORPUS="${DATA_DIR}/news/Apw.conll"
+#> NYT
+elif [ $SEED == 33 ]; then
+    # echo "  Task ID ${SEED} assigned to 'Nyt1' dataset"
+    SEED_CORPUS="${DATA_DIR}/news/Nyt1.conll"
+elif [ $SEED == 34 ]; then
+    # echo "  Task ID ${SEED} assigned to 'Nyt2' dataset"
+    SEED_CORPUS="${DATA_DIR}/news/Nyt2.conll"
+
+#* #HACK test
+elif [ $SEED == 35 ]; then
+    SEED_CORPUS="${OUT_DIR}/corpora_shortcuts/small_test"
+
+elif [ $SEED == 36 ]; then
+    SEED_CORPUS="${OUT_DIR}/corpora_shortcuts/large_test"
+
+
+#* puddin
+elif [ $SEED == 30 ]; then
+    # echo "  Task ID ${SEED} assigned to 'test' dataset"
+    SEED_CORPUS="${PUDDIN_CORPUS}/PccTe.conll"
 elif [ $SEED == 31 ]; then
-    echo "  Task ID ${SEED} assigned to 'val' dataset"
-    SEED="Va"
+    # echo "  Task ID ${SEED} assigned to 'val' dataset"
+    SEED_CORPUS="${PUDDIN_CORPUS}/PccVa.conll"
 else
     SEEDL=${#SEED}
     if [ $SEEDL -lt 2 ]; then
         SEED="0${SEED}"
     fi
-    echo "  ${SEED} dataset"
+    # echo "  ${SEED} dataset"
+    SEED_CORPUS ="${PUDDIN_CORPUS}/Pcc${SEED}.conll"
 fi
 
-SEED_CORPUS=${DATA_DIR}/puddin/Pcc${SEED}.conll
-# echo "  ${SEED_CORPUS}"
-# echo "  ${PAT_DIR}"
+echo "Processing..."
+echo "Corpus ${SEED}:  ${SEED_CORPUS}"
+echo "Pattern Type:  ${PAT_DIR}"
 
 if [[ -d $SEED_CORPUS && -d ${PAT_DIR} ]]; then
     # run script and send both stdout and stderr to log file
     DATE="$(date -I)"
-    LOG_FILE=${LOGS_DIR}/${SLURM_JOB_NAME}${SEED}_${DATE}.log
+    LOG_FILE=${LOGS_DIR}/${SLURM_JOB_NAME}-${SEED}_${DATE}.log
     echo "Combined log will be appended to ${LOG_FILE}"
 
-    echo "time python ${SOURCE_DIR}/run_pipeline.py -c ${SEED_CORPUS} -p ${PAT_DIR}"
-    time python ${SOURCE_DIR}/run_pipeline.py -c ${SEED_CORPUS} -p ${PAT_DIR} >> >(tee -i -a ${LOG_FILE}) 2>&1
+    echo -e "time python ${SOURCE_DIR}/run_pipeline.py -c ${SEED_CORPUS} -p ${PAT_DIR}\n-g ${OUT_DIR}/1_json_grew-matches >> >(tee -i -a ${LOG_FILE}) 2>&1"
+    time python ${SOURCE_DIR}/run_pipeline.py -c ${SEED_CORPUS} -p ${PAT_DIR} -g ${OUT_DIR}/1_json_grew-matches >> >(tee -i -a ${LOG_FILE}) 2>&1
 
 else
-    echo "SEED values ${SEED} do not point to existing directories. Skipping."
+    echo "SEED value ${SEED} does not point to existing directories. Skipping."
 
 fi
 echo "Finished @ $(date)"
