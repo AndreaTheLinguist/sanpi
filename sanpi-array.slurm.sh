580bd9cea62bb66c48b4f07fb284db08697827c6
Andrea Hummel <andrea.r.hummel@gmail.com>
2023-03-13 23:40:05 -0400

    additional array definitions for extended testing
    more sophisticated options for `run_..` glue script

--- sanpi-array.slurm.sh
+++ sanpi-array.slurm.sh
@@ -2,16 +2,37 @@
 #SBATCH --mail-user=arh234@cornell.edu
 #SBATCH --mail-type=ALL
 #SBATCH -J contig-puddin         # Job name
-#SBATCH -o %x_%2a.out                # Name of stdout output log file (%j expands to jobID)
-#SBATCH -e %x_%2a.err                # Name of stderr output log file (%j expands to jobID)
+#SBATCH -o full-%x_%2a.%A.out                # Name of stdout output log file (%j expands to jobID)
+#SBATCH -e full-%x_%2a.%A.err                # Name of stderr output log file (%j expands to jobID)
 #SBATCH --open-mode=append
-#SBATCH --array 0-31
+#SBATCH --array 0-34
 #SBATCH -N 1                            # Total number of nodes requested
+##SBATCH -n 12                            # Total number of cores requested
+##SBATCH --mem-per-cpu=16G                     # Total amount of (real) memory requested (per node)
 #SBATCH -n 1                            # Total number of cores requested
-#SBATCH --mem=11G                     # Total amount of (real) memory requested (per node)
-#SBATCH --time 12:00:00                  # Time limit (hh:mm:ss)
+#SBATCH --mem=60G
+#SBATCH --time 48:00:00                  # Time limit (hh:mm:ss)
 #SBATCH --get-user-env
 #SBATCH --chdir=/share/compling/projects/sanpi/logs # to allow running `sbatch` cmd from anywhere and still put .out and .err in ../logs/ dir
+
+# * Array Assignments * # 
+# > puddin
+# 0-29  ../puddin/Pcc[##].conll
+#   OR
+#   30  ../puddin/PccTe.conll
+#   31  ../puddin/PccVa.conll
+#
+# > news
+# ## APW ##
+#   32  ../news/Apw.conll
+#
+# ## NYT ##
+#   33  ../news/Nyt1.conll
+#   34  ../news/Nyt2.conll
+#
+# > #HACK testing
+#   35  ../sanpi/corpora_shortcuts/testing/small.conll
+#   36  ../sanpi/corpora_shortcuts/testing/large.conll
 
 set -o errexit
 echo ">>=======================================<<"
@@ -22,11 +43,12 @@ echo ""
 # activate conda environment
 eval "$(conda shell.bash hook)"
 conda activate parallel-sanpi
-conda info | head -2 | tail -1
+echo "python interpreter path:"
+which python
 
 # pattern directory should be specified as directory name
 PAT_DIR_NAME=${1:-"contig"}
-echo "Pattern Type: ${PAT_DIR_NAME##*/}"
+echo "Pattern Type: \'${PAT_DIR_NAME##*/}\'"
 DATA_DIR=/share/compling/data
 if [[ ! -d ${DATA_DIR} ]]; then
     DATA_DIR=/home/arh234/data
@@ -45,7 +67,7 @@ if [[ ! -d "${LOGS_DIR}" ]]; then
     mkdir ${LOGS_DIR}
 fi
 
-echo "Job $SLURM_JOB_NAME - $SLURM_JOB_ID"
+echo "Job \'$SLURM_JOB_NAME\' - $SLURM_JOB_ID"
 echo "  running on:"
 echo "   - partition: $SLURM_JOB_PARTITION"
 echo "   - node: $SLURM_JOB_NODELIST"
@@ -57,29 +79,38 @@ PAT_DIR=${SOURCE_DIR}/Pat/${PAT_DIR_NAME}
 SEED=$((SLURM_ARRAY_TASK_ID))
 echo "Array Index = ${SEED}"
 
+NEWS_DIR="$DATA_DIR/news"
+PUDDIN_CORPUS="$DATA_DIR/puddin"
+
 ## * Assign Array Job Inputs * ##
 # * news
-#> APW
+## APW ##
 if [ $SEED == 32 ]; then
     # echo "  Task ID ${SEED} assigned to 'Apw' dataset"
-    SEED_CORPUS="${DATA_DIR}/news/Apw.conll"
-#> NYT
+    SEED_CORPUS="${NEWS_DIR}/Apw.conll"
+
+## NYT ##
 elif [ $SEED == 33 ]; then
     # echo "  Task ID ${SEED} assigned to 'Nyt1' dataset"
-    SEED_CORPUS="${DATA_DIR}/news/Nyt1.conll"
+    SEED_CORPUS="${NEWS_DIR}/Nyt1.conll"
 elif [ $SEED == 34 ]; then
     # echo "  Task ID ${SEED} assigned to 'Nyt2' dataset"
-    SEED_CORPUS="${DATA_DIR}/news/Nyt2.conll"
+    SEED_CORPUS="${NEWS_DIR}/Nyt2.conll"
 
-#* #HACK test
+# * #HACK test
 elif [ $SEED == 35 ]; then
-    SEED_CORPUS="${OUT_DIR}/corpora_shortcuts/small_test"
+    SEED_CORPUS="${OUT_DIR}/corpora_shortcuts/testing/small.conll"
 
 elif [ $SEED == 36 ]; then
-    SEED_CORPUS="${OUT_DIR}/corpora_shortcuts/large_test"
+    SEED_CORPUS="${OUT_DIR}/corpora_shortcuts/testing/large.conll"
 
+elif [ $SEED == 37 ]; then
+    SEED_CORPUS="${OUT_DIR}/corpora_shortcuts/testing/smallest20.conll"
 
-#* puddin
+elif [ $SEED == 38 ]; then
+    SEED_CORPUS="${OUT_DIR}/corpora_shortcuts/testing/midrange10.conll"
+
+# * puddin
 elif [ $SEED == 30 ]; then
     # echo "  Task ID ${SEED} assigned to 'test' dataset"
     SEED_CORPUS="${PUDDIN_CORPUS}/PccTe.conll"
@@ -92,12 +123,12 @@ else
         SEED="0${SEED}"
     fi
     # echo "  ${SEED} dataset"
-    SEED_CORPUS ="${PUDDIN_CORPUS}/Pcc${SEED}.conll"
+    SEED_CORPUS="${PUDDIN_CORPUS}/Pcc${SEED}.conll"
 fi
 
 echo "Processing..."
 echo "Corpus ${SEED}:  ${SEED_CORPUS}"
-echo "Pattern Type:  ${PAT_DIR}"
+echo "Pattern Directory:  ${PAT_DIR}"
 
 if [[ -d $SEED_CORPUS && -d ${PAT_DIR} ]]; then
     # run script and send both stdout and stderr to log file
