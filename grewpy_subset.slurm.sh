f7cf0f33e74812db5e9823ace9c2426a44de70ad
Andrea Hummel <andrea.r.hummel@gmail.com>
2023-04-05 21:50:08 -0400

    upped the `--mem`  request to `16G`

--- grewpy_subset.slurm.sh
+++ grewpy_subset.slurm.sh
@@ -7,7 +7,7 @@
 ##SBATCH --open-mode=append
 #SBATCH --nodes=1               # Total number of nodes requested
 #SBATCH --ntasks=1              # Total number of tasks (defaults to 1 cpu/task, but overrride with -c)
-#SBATCH --mem=15G               # Total amount of (real) memory requested (per node)
+#SBATCH --mem=16G               # Total amount of (real) memory requested (per node)
 #SBATCH --time 1:00:00          # Time limit (hh:mm:ss)
 #SBATCH --get-user-env
 #SBATCH --chdir=/share/compling/projects/sanpi/logs/grewpy_subsets     # change working directory to this before execution
