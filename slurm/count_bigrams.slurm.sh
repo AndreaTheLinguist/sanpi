16fb5b341749057fef17aa3d94b880fd447e46c2
Andrea Hummel <andrea.r.hummel@gmail.com>
2023-07-03 21:21:21 -0400

    slurm script for submitting jobs to run `count_bigrams.py`

---
+++ slurm/count_bigrams.slurm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -N1
+#SBATCH -n1
+#SBATCH --mem=16G
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+#SBATCH --time 3:00:00
+#SBATCH -J count_bigrams
+#SBATCH --requeue
+#SBATCH --chdir=/share/compling/projects/sanpi/logs
+
+# usage:    sbatch [slurm flags] count_bigrams.slurm.sh [N_FILES] [THRESH]
+echo 'running slurm script: /share/compling/projects/sanpi/slurm/count_bigrams.slurm.sh'
+echo "JOB ID: ${SLURM_JOB_ID}"
+echo "JOB NAME: ${SLURM_JOB_NAME}"
+echo "started @ $(date '+%F %X') from $(pwd)"
+echo ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ."
+echo "running on ${SLURM_JOB_NODELIST} with `nproc` cores"
+echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+
+eval "$(conda shell.bash hook)"
+conda activate sanpi
+
+N_FILES=${1:-''}
+THRESH=${2:-''}
+PROG="/share/compling/projects/sanpi/source/analyze/count_bigrams.py"
+LOG_FILE="${SLURM_JOB_NAME}.${SLURM_JOB_ID}.log"
+echo 'time python $PROG -f $N_FILES -t $THRESH'
+echo "time python $PROG -f $N_FILES -t $THRESH"
+time python $PROG -f $N_FILES -t $THRESH >> >(tee -i -a ${LOG_FILE}) 2>&1
