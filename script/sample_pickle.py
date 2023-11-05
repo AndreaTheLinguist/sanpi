6fef34caa5b41505374bfd7d2e55e2a4feed463e
Andrea Hummel <andrea.r.hummel@gmail.com>
2023-11-05 15:08:23 -0500

    updated `sample_pickle.py` to optionally: - filter rows by values for given column(s) - sort by specific column (or index by default) also added error handling/warning messages
    ```log
    usage: sample_pickle.py [-h] [-N SAMPLE_SIZE] [-s SORT_BY] [-c COLUMNS] [-f FILTERS] [-m] pickle
    
    simple script to print a sample of a pickled dataframe to stdout as either the default `pandas`
    output or as a markdown table (-m). Specific columns can be selected (defaults to all columns),
    and sample size can be dictated (defaults to 20 rows)
    
    positional arguments:
      pickle                path to pickled dataframe
    
    options:
      -h, --help            show this help message and exit
      -N SAMPLE_SIZE, --sample_size SAMPLE_SIZE
                            number of rows to include in sample (default: 20)
      -s SORT_BY, --sort_by SORT_BY
                            name of column to sort sample by; needn't be selected for printout. *note*
                            this will be _ascending_ (A-Z or increasing numerical values) (default:
                            None)
      -c COLUMNS, --column COLUMNS
                            option to specify columns to print. Each must have its own `-c` flag. E.g.
                            `-c COLUMN_1 -c COLUMN_2` (default: [])
      -f FILTERS, --filter FILTERS
                            option to filter rows before sampling. Specify as a string of the format:
                            `COLUMN_NAME==VALUE` or `COLUMN_NAME!=VALUE` to invert the filter. For
                            example, to limit the sample to only rows with the adverb "absolutely",
                            use → `adv_lemma==absolutely` or `adv_form==absolutely` To only see rows
                            where the adjective is NOT "good", use → `adj_lemma!=good` or
                            `adj_form!=good`. As with the --column flag, there can be multiple
                            filters, but every string needs its own flag. NOTE: row filtering is done
                            *before* column selection so filters may be based on columns which will
                            not be printed. (default: [])
      -m, --markdown        option to print in markdown table format (default: False)
    ```

--- script/sample_pickle.py
+++ script/sample_pickle.py
@@ -5,47 +5,126 @@ from pathlib import Path
 import pandas as pd
 
 
-def _sample_pickle():
+def print_sample():
     """
-    Prints a given sample size of a pickled dataframe to stdout as a markdown table or default `pandas` output.
+    prints a sample from a pickled DataFrame based on command line arguments.
+    """
+    
+    args = _parse_args()
+
+    #TODO: extend this to work with any type of tabular data input (or at least `.csv` and `.psv` files)
+    data = pd.read_pickle(args.pickle)
+    
+    data, filtered = filter_rows(data, args.filters)
+
+    sampled = data.sample(min(len(data), args.sample_size))
+    if args.sort_by: 
+        sampled = sampled.sort_values(args.sort_by)
+    else:
+        sampled = sampled.sort_index()
+    
+    sampled = select_columns(sampled, args.columns)
+    
+    length_info = (f'{len(sampled)} random rows' if len(sampled) < len(data) 
+                  else f'All ({len(data)}) rows')
+    if filtered: 
+        length_info += ' matching filter(s)'
+    print(f'\n## {length_info} from `{args.pickle.name}`\n')
+    
+    if args.markdown:
+        print(sampled.to_markdown(floatfmt=',.0f'))
+    else:
+        print(sampled)
+
+def filter_rows(input_data:pd.DataFrame, 
+                filter_list:list):
+    """
+        Filters rows of a pandas DataFrame based on a list of filter expressions.
+
+        Args:
+            input_data: The pandas DataFrame to filter.
+            filter_list: A list of filter expressions in the format "column_name=filter_value" or "column_name!=filter_value".
+
+        Returns:
+            Tuple[pd.DataFrame, bool]: A tuple containing the filtered DataFrame and a boolean indicating if any rows were filtered.
+
+        Raises:
+            None
+
+        Examples:
+            >>> filter_rows(pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]}), ["A=1", "B!=2"])
+            (pd.DataFrame({"A": [1, 3], "B": [4, 6]}), True)
+    """
+    filtered = False
+    if filter_list: 
+        print('')
+        f = input_data.copy()
+        for filter_str in filter_list: 
+            filter_col, filter_val = filter_str.rsplit('=',1)
+            op = filter_col[-1]
+            filter_col = filter_col[:-1]
+            try: 
+                target_param = f[filter_col]
+            except KeyError: 
+                print(f'- ✕ ERROR: Filter column `{filter_col}` not found. Filter `{filter_str}` ignored.')
+                continue
+            
+            # `filter_col` NOT equal to `filter_val`
+            if op == '!': 
+                f = f.loc[target_param != filter_val, :]
+
+                # `filter_col` IS equal to `filter_val`
+            elif op == '=': 
+                f = f.loc[target_param == filter_val, :]
+                    
+            if f.empty: 
+                print(f'- Filter expression "{filter_str}" matched zero rows. Filter not applied.')
+                f = input_data.copy()
+            else: 
+                filtered = True
+                input_data = f
+                print(f'- ✓ Applied filter: `{filter_str}`')
+                
+    return input_data, filtered
+
+def select_columns(data_selection: pd.DataFrame, 
+                   seek_cols: list):
+    """
+    Selects columns from a pandas DataFrame based on a list of column names.
 
     Args:
-        None
+        data_selection: The pandas DataFrame to select columns from.
+        seek_cols: A list of column names to select.
 
     Returns:
-        None
+        pd.DataFrame: The pandas DataFrame with only the selected columns.
 
     Raises:
         None
 
+    Examples:
+        >>> select_columns(pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]}), ["A"])
+        pd.DataFrame({"A": [1, 2, 3]})
     """
-    args = _parse_args()
 
-    d = pd.read_pickle(args.pickle)
-    s = d.sample(min(len(d), args.sample_size))
-    if args.columns:
-        s = s[args.columns]
-    length_info = (f'{len(s)} random' if len(s) < len(d) 
-                  else f'All rows ({len(d)})')
-    print(f'## {length_info} rows from `{args.pickle.name}`\n')
-    if args.markdown:
-        print(s.to_markdown(floatfmt=',.0f'))
-    else:
-        print(s)
+    if seek_cols: 
+        selected_cols = []
+        for col in seek_cols: 
+            if col not in data_selection.columns: 
+                print(f'\n- Warning‼️ "{col}" not in columns. Selection ignored.')
+            else: 
+                selected_cols.append(col)
+        if selected_cols: 
+            data_selection = data_selection[selected_cols]
+    return data_selection
 
 
 def _parse_args():
     """
     Parses the command line arguments.
-
-    Args:
-        None
-
+    
     Returns:
         argparse.Namespace: The parsed command line arguments.
-
-    Raises:
-        None
 
     """
     parser = argparse.ArgumentParser(
@@ -54,12 +133,21 @@ def _parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument('pickle',
-                        type=Path, help=('path to pickled dataframe'))
+                        type=Path, 
+                        help=('path to pickled dataframe')
+                        )
 
     parser.add_argument(
-        '-s', '--sample_size',
-        type=int, default=20, action=None,
+        '-N', '--sample_size',
+        type=int, default=20,
         help=('number of rows to include in sample'))
+
+    parser.add_argument(
+        '-s', '--sort_by',
+        type=str, default=None,
+        help=('name of column to sort sample by; '
+              "needn't be selected for printout. "
+              '*note* this will be _ascending_ (A-Z or increasing numerical values)'))
     
     parser.add_argument(
         '-c', '--column',
@@ -68,14 +156,31 @@ def _parse_args():
         help=('option to specify columns to print. Each must have its own `-c` flag. E.g. `-c COLUMN_1 -c COLUMN_2`')
     )
     parser.add_argument(
+        '-f', '--filter',
+        type=str, action='append', dest='filters',
+        default=[],
+        help=('option to filter rows before sampling. '
+              'Specify as a string of the format: '
+              '`COLUMN_NAME==VALUE` or `COLUMN_NAME!=VALUE` to invert the filter. '
+              'For example, to limit the sample to only rows with the adverb '
+              '"absolutely", use → `adv_lemma==absolutely` or `adv_form==absolutely` '
+              'To only see rows where the adjective is NOT "good", use → '
+              '`adj_lemma!=good` or `adj_form!=good`. '
+              'As with the --column flag, there can be multiple '
+              'filters, but every string needs its own flag. '
+              'NOTE: row filtering is done *before* column selection '
+              'so filters may be based on columns which will not be printed.')
+    )
+
+    parser.add_argument(
         '-m', '--markdown',
         action='store_true',
         default=False,
         help=('option to print in markdown table format')
     )
-
+    
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-    _sample_pickle()
+    print_sample()
