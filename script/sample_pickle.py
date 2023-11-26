983d772f37ae0596f2dbaafb63f5c3bc94fc9b29
Andrea Hummel <andrea.r.hummel@gmail.com>
2023-11-26 13:46:32 -0500

    # reconfigured `./script/sample_pickle.py` to import `sample_pickle` method from `./source/utils/sample.py` ➡️ `from utils.sample import sample_pickle`
    ## `sample.sample_pickle()` method now:
    + returns a dataframe,
    + can print in various tabular formats, e.g. `.csv`, `.psv`, etc.
      (which can be redirected to a file if desired)
    + can have printing meta messages turned of (`quiet=True`)
       - necessary for creating a sample file via cli redirect,
          e.g.
          ```shell
          python ../sanpi/script/sample_pickle.py \
             -p path/to/df.xxx --quiet --comma \
             > df-sample.csv
          ```
    + can suppress _all printing_, e.g. table as well, not just messages
       via argument `print_sample=False`
    following docstring has been added:
    
    ```python
    """
    Sample and print a table of data.
    
    Args:
        data_path (Path): The path to the data file. Defaults to '/share/compling/data/sanpi/4_post-processed/RBdirect/neg-bigrams_thr0-001p.10f.pkl.gz'.
        sample_size (int): The number of rows to sample. Defaults to 20.
        sort_by (str): The column to sort the data by. Defaults to an empty string.
        columns (list): The columns to include in the table. Defaults to None.
        filters (list): The filters to apply to the data. Defaults to None.
        markdown (bool): Whether to format the table as markdown. Defaults to False.
        transpose (bool): Whether to transpose the table. Defaults to False.
        max_cols (int): The maximum number of columns to display. Defaults to 20.
        max_colwidth (int): The maximum width of each column. Defaults to 40.
        max_width (int): The maximum width of the table. Defaults to 100.
        tabbed (bool): Whether to format the table as tab-separated values. Defaults to False.
        comma (bool): Whether to format the table as comma-separated values. Defaults to False.
        piped (bool): Whether to format the table as piped values. Defaults to False.
        quiet (bool): Whether to suppress printing the table. Defaults to False.
        print_sample (bool): Whether to print the sampled table. Defaults to True.
    
    Returns:
        pd.DataFrame: The sampled data table.
    """
    ```

--- script/sample_pickle.py
+++ script/sample_pickle.py
@@ -2,195 +2,36 @@
 import argparse
 from pathlib import Path
 
-import pandas as pd
-
-from utils import set_pd_display
 
 # pd.set_option('display.max_colwidth', 40)
 # pd.set_option('display.max_columns', 20)
 # pd.set_option('display.width', 200)
+try:
+    from utils import sample_pickle
+except ModuleNotFoundError:
+    from source.utils import sample_pickle
 
 
-def print_sample():
-    """
-    prints a sample from a pickled DataFrame based on command line arguments.
-    """
+def _main():
+    # [-N SAMPLE_SIZE] [-s SORT_BY] [-c COLUMNS] [-f FILTERS] [-m] [-t] [-C MAX_COLS] [-w MAX_COLWIDTH] [-W MAX_WIDTH] pickle
+
     args = _parse_args()
-    max_cols = set_max_cols(args)
 
-    set_pd_display(max_colwidth=args.max_colwidth,
-                   max_width=args.max_width,
-                   max_cols=max_cols)
-
-    data_sample = _get_data_sample(args)
-
-    _print_table(data_sample, max_cols, 
-                 args.transpose, args.markdown)
-
-
-def set_max_cols(args):
-    read_path = args.pickle
-    if not read_path.is_file():
-        exit(f'Error: Input file not found:\n ✕ {read_path}')
-    max_cols = args.max_cols
-    col_list = args.columns
-    if args.transpose:
-        max_cols = args.sample_size or max_cols
-    elif col_list:
-        max_cols = len(col_list)
-    return max_cols
-
-
-def _get_data_sample(args):
-    sample_size = args.sample_size
-    full_frame = _read_data(args.pickle)
-    # [x]: extend this to work with any type of tabular data input (or at least `.csv` and `.psv` files)
-
-    filtered_data, filter_applied = _filter_rows(full_frame, args.filters)
-    if sample_size:
-        data = filtered_data.sample(min(len(filtered_data), sample_size))
-    if args.sort_by:
-        data = data.sort_values(args.sort_by)
-    else:
-        data = data.sort_index()
-
-    data = _select_columns(data, args.columns)
-
-    _print_header(len(data), len(filtered_data),
-                  filter_applied, args.pickle.name)
-
-    return data
-
-
-def _read_data(read_path):
-    read_suffix = read_path.suffix
-    if '.pkl' in read_path.suffixes:
-        full_data = pd.read_pickle(read_path)
-    elif read_suffix == '.csv':
-        full_data = pd.read_csv(read_path)
-    elif read_suffix == '.psv':
-        full_data = pd.read_csv(read_path, delimiter='|')
-    elif read_suffix == '.tsv':
-        full_data = pd.read_csv(read_path, delimiter='\t')
-    elif read_suffix == '.json':
-        full_data = pd.read_json(read_path)
-    else:
-        exit(f'Error: Input file suffix, {read_suffix}, is either '
-             + 'not interpretable or not an expected suffix for a DataFrame.')
-    print(f'\n## Sampling from `{read_path}`')
-    return full_data
-
-
-def _print_header(n_sample_rows, n_input_rows, file_name: str,
-                  filter_applied: bool):
-
-    length_info = (f'{n_sample_rows} random row{"s" if n_sample_rows != 1 else ""}'
-                   if n_sample_rows < n_input_rows
-                   else f'All ({n_sample_rows}) row(s)')
-    if filter_applied:
-        length_info += ' matching filter(s)'
-    print(f'\n### {length_info} from `{file_name}`\n')
-
-
-def _filter_rows(input_data: pd.DataFrame,
-                filter_list: list):
-    """
-        Filters rows of a pandas DataFrame based on a list of filter expressions.
-
-        Args:
-            input_data: The pandas DataFrame to filter.
-            filter_list: A list of filter expressions in the format "column_name=filter_value" or "column_name!=filter_value".
-
-        Returns:
-            Tuple[pd.DataFrame, bool]: A tuple containing the filtered DataFrame and a boolean indicating if any rows were filtered.
-
-        Raises:
-            None
-
-        Examples:
-            >>> filter_rows(pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]}), ["A=1", "B!=2"])
-            (pd.DataFrame({"A": [1, 3], "B": [4, 6]}), True)
-    """
-    filtered = False
-    if filter_list:
-        print('\n- *filtering rows...*')
-        f = input_data.copy()
-        for filter_str in filter_list:
-            filter_col, filter_val = filter_str.rsplit('=', 1)
-            op = filter_col[-1]
-            filter_col = filter_col[:-1]
-            try:
-                target_param = f[filter_col]
-            except KeyError:
-                print(
-                    f'  - ✕ ERROR: Filter column `{filter_col}` not found. Filter `{filter_str}` ignored.')
-                continue
-
-            # `filter_col` NOT equal to `filter_val`
-            if op == '!':
-                f = f.loc[target_param != filter_val, :]
-
-                # `filter_col` IS equal to `filter_val`
-            elif op == '=':
-                f = f.loc[target_param == filter_val, :]
-
-            if f.empty:
-                print(
-                    f'  - Filter expression `{filter_str}` matched zero rows. Filter not applied.')
-                f = input_data.copy()
-            else:
-                filtered = True
-                input_data = f
-                print(f'  - ✓ Applied filter: `{filter_str}`')
-
-    return input_data, filtered
-
-
-def _select_columns(data_selection: pd.DataFrame,
-                   seek_cols: list):
-    """
-    Selects columns from a pandas DataFrame based on a list of column names.
-
-    Args:
-        data_selection: The pandas DataFrame to select columns from.
-        seek_cols: A list of column names to select.
-
-    Returns:
-        pd.DataFrame: The pandas DataFrame with only the selected columns.
-
-    Raises:
-        None
-
-    Examples:
-        >>> select_columns(pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]}), ["A"])
-        pd.DataFrame({"A": [1, 2, 3]})
-    """
-
-    if seek_cols:
-        print('\n- *selecting columns...*')
-        selected_cols = []
-        for col in seek_cols:
-            if col not in data_selection.columns:
-                print(
-                    f'  - Warning‼️ `{col}` not in columns. Selection ignored.')
-            else:
-                selected_cols.append(col)
-        if selected_cols:
-            data_selection = data_selection[selected_cols]
-    return data_selection
-
-
-def _print_table(data_sample, max_cols, transpose, markdown):
-    if transpose:
-        print('_note: table transposed for display_\n')
-        data_sample = data_sample.transpose()
-    if markdown:
-        data_sample = data_sample.iloc[:, 0:max_cols]
-        print(data_sample.to_markdown(floatfmt=',.0f'))
-    else:
-        print('```')
-        print(data_sample)
-        print('```')
+    sample_pickle(
+        data_path=args.path,
+        sample_size=args.sample_size,
+        sort_by=args.sort_by,
+        columns=args.columns,
+        filters=args.filters,
+        markdown=args.markdown,
+        transpose=args.transpose,
+        max_cols=args.max_cols,
+        max_colwidth=args.max_colwidth,
+        max_width=args.max_width,
+        tabbed=args.tabbed,
+        comma=args.comma,
+        piped=args.piped,
+        quiet=args.quiet)
 
 
 def _parse_args():
@@ -210,24 +51,29 @@ def _parse_args():
             'Tip: use `-N 1 -t` to see example of all included columns'),
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument('pickle',
-                        type=Path,
-                        help=('path to pickled dataframe')
-                        )
+    parser.add_argument(
+        '-p', '--path',
+        type=Path,
+        default=Path(
+            '/share/compling/data/sanpi/4_post-processed/RBdirect/neg-bigrams_thr0-001p.10f.pkl.gz'),
+        help=('path to dataframe to sample')
+    )
 
     parser.add_argument(
         '-N', '--sample_size',
         type=int, default=20,
         help=('number of rows to include in sample. '
               'To disable sampling (show entire '
-              'table--use with caution! ⚠️), use `-N 0`'))
+              'table--use with caution! ⚠️), use `-N 0`')
+    )
 
     parser.add_argument(
         '-s', '--sort_by',
         type=str, default=None,
         help=('name of column to sort sample by; '
               "needn't be selected for printout. "
-              '*note* this will be _ascending_ (A-Z or increasing numerical values)'))
+              '*note* this will be _ascending_ (A-Z or increasing numerical values)')
+    )
 
     parser.add_argument(
         '-c', '--column',
@@ -262,6 +108,25 @@ def _parse_args():
     )
 
     parser.add_argument(
+        '-T', '--tabbed',
+        action='store_true',
+        default=False,
+        help=('option to print tab-delimited format')
+    )
+    parser.add_argument(
+        '-P', '--piped',
+        action='store_true',
+        default=False,
+        help=('option to print pipe-delimited (|) format')
+    )
+    parser.add_argument(
+        '-C', '--comma',
+        action='store_true',
+        default=False,
+        help=('option to print in csv format')
+    )
+
+    parser.add_argument(
         '-t', '--transpose',
         action='store_true',
         default=False,
@@ -269,27 +134,41 @@ def _parse_args():
     )
 
     parser.add_argument(
-        '-C', '--max_cols',
+        '-q', '--quiet',
+        action='store_true',
+        default=False,
+        help=('option to suppress meta-messages printed to stdout. '
+              '(Can be used with `-T/--tabbed` or `-P/--piped` or `-C/--comma` '
+              'to write sample to file or use with `column -T [-s,/-s\\|]`)')
+    )
+
+    parser.add_argument(
+        '-x', '--max_cols',
         type=int, default=20,
         help=('max number of columns to include in sample; overriden by '
-              '--column flags if given (or --sample_size if --transpose specified)'))
+              '--column flags if given (or --sample_size if --transpose specified)')
+    )
 
     parser.add_argument(
         '-w', '--max_colwidth',
         type=int, default=40,
         help=('max width (in pixels?) of each column in sample display; '
               '*currently does not apply to markdown formatted displays, '
-              'which include full values for every included cell.'))
+              'which include full values for every included cell.')
+    )
 
     parser.add_argument(
         '-W', '--max_width',
         type=int, default=180,
         help=('max width of the entire display; '
               '*currently does not apply to markdown formatted displays, '
-              'which will be the sum of the widest cell in every included columm'))
+              'which will be the sum of the widest cell in every included columm')
+    )
+
+
 
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-    print_sample()
+    _main()
---
+++ source/utils/sample.py
@@ -0,0 +1,249 @@
+# coding=utf-8
+import contextlib
+from pathlib import Path
+
+import pandas as pd
+try:
+    from utils import set_pd_display, print_md_table  # pylint: disable=import-error
+except ModuleNotFoundError:
+    with contextlib.suppress(ModuleNotFoundError):
+        from source.utils import set_pd_display, print_md_table
+
+
+def sample_pickle(data_path: Path = Path('/share/compling/data/sanpi/4_post-processed/RBdirect/neg-bigrams_thr0-001p.10f.pkl.gz'),
+                  sample_size: int = 20,
+                  sort_by: str = '',
+                  columns: list = None,
+                  filters: list = None,
+                  markdown: bool = False,
+                  transpose: bool = False,
+                  max_cols: int = 20,
+                  max_colwidth: int = 40,
+                  max_width: int = 100,
+                  tabbed: bool = False,
+                  comma: bool = False,
+                  piped: bool = False,
+                  quiet: bool = False,
+                  print_sample: bool = True):
+    
+    """
+    Sample and print a table of data.
+
+    Args:
+        data_path (Path): The path to the data file. Defaults to '/share/compling/data/sanpi/4_post-processed/RBdirect/neg-bigrams_thr0-001p.10f.pkl.gz'.
+        sample_size (int): The number of rows to sample. Defaults to 20.
+        sort_by (str): The column to sort the data by. Defaults to an empty string.
+        columns (list): The columns to include in the table. Defaults to None.
+        filters (list): The filters to apply to the data. Defaults to None.
+        markdown (bool): Whether to format the table as markdown. Defaults to False.
+        transpose (bool): Whether to transpose the table. Defaults to False.
+        max_cols (int): The maximum number of columns to display. Defaults to 20.
+        max_colwidth (int): The maximum width of each column. Defaults to 40.
+        max_width (int): The maximum width of the table. Defaults to 100.
+        tabbed (bool): Whether to format the table as tab-separated values. Defaults to False.
+        comma (bool): Whether to format the table as comma-separated values. Defaults to False.
+        piped (bool): Whether to format the table as piped values. Defaults to False.
+        quiet (bool): Whether to suppress printing meta-messages. Defaults to False.
+        print_sample (bool): Whether to print the sampled table. Defaults to True.
+
+    Returns:
+        pd.DataFrame: The sampled data table.
+    """
+    
+    if transpose:
+        max_cols = sample_size or max_cols
+    elif columns:
+        max_cols = len(columns)
+    if not (tabbed or piped or markdown or comma):
+        set_pd_display(max_colwidth=max_colwidth,
+                       max_width=max_width,
+                       max_cols=max_cols)
+
+    data_sample = _get_data_sample(sample_size=sample_size, data_path=data_path, filters=filters,
+                                   columns=columns, sort_by=sort_by, quiet=quiet)
+
+    if print_sample:
+        _print_table(data_sample=data_sample,
+                     max_cols=max_cols, transpose=transpose, quiet=quiet,
+                     markdown=markdown, tabbed=tabbed, comma=comma, piped=piped)
+
+    return data_sample
+
+
+def _get_data_sample(sample_size: int,
+                     data_path: Path,
+                     filters: list = None,
+                     columns: list = None,
+                     sort_by: str = '',
+                     quiet: bool = False):
+
+    full_frame = _read_data(data_path, quiet)
+
+    filtered_data, filter_applied = _filter_rows(full_frame, filters, quiet)
+    if sample_size:
+        data = filtered_data.sample(min(len(filtered_data), sample_size))
+    data = data.sort_values(sort_by) if sort_by else data.sort_index()
+    data = _select_columns(data, columns, quiet)
+    if not quiet:
+        _print_header(len(data), len(filtered_data),
+                      data_path.name, filter_applied)
+
+    return data
+
+
+def _read_data(read_path, quiet=False):
+
+    if not read_path.is_file():
+        exit(f'Error: Input file not found:\n ✕ {read_path}')
+
+    read_suffix = read_path.suffix
+    if '.pkl' in read_path.suffixes:
+        full_data = pd.read_pickle(read_path)
+    elif read_suffix == '.csv':
+        full_data = pd.read_csv(read_path)
+    elif read_suffix == '.psv':
+        full_data = pd.read_csv(read_path, delimiter='|')
+    elif read_suffix == '.tsv':
+        full_data = pd.read_csv(read_path, delimiter='\t')
+    elif read_suffix == '.json':
+        full_data = pd.read_json(read_path)
+    else:
+        exit(f'Error: Input file suffix, {read_suffix}, is either '
+             + 'not interpretable or not an expected suffix for a DataFrame.')
+    if not quiet:
+        print(f'\n## Sampling from `{read_path}`')
+    return full_data
+
+
+def _print_header(n_sample_rows, n_input_rows, file_name: str,
+                  filter_applied: bool):
+
+    length_info = (f'{n_sample_rows} random row{"s" if n_sample_rows != 1 else ""}'
+                   if n_sample_rows < n_input_rows
+                   else f'All ({n_sample_rows}) row(s)')
+    if filter_applied:
+        length_info += ' matching filter(s)'
+    print(f'\n### {length_info} from `{file_name}`\n')
+
+
+def _filter_rows(input_data: pd.DataFrame,
+                 filter_list: list,
+                 quiet=False):
+    """
+        Filters rows of a pandas DataFrame based on a list of filter expressions.
+
+        Args:
+            input_data: The pandas DataFrame to filter.
+            filter_list: A list of filter expressions in the format "column_name=filter_value" or "column_name!=filter_value".
+
+        Returns:
+            Tuple[pd.DataFrame, bool]: A tuple containing the filtered DataFrame and a boolean indicating if any rows were filtered.
+
+        Raises:
+            None
+
+        Examples:
+            >>> filter_rows(pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]}), ["A=1", "B!=2"])
+            (pd.DataFrame({"A": [1, 3], "B": [4, 6]}), True)
+    """
+    filtered = False
+    if filter_list:
+        if not quiet:
+            print('\n- *filtering rows...*')
+        f = input_data.copy()
+        for filter_str in filter_list:
+            filter_col, filter_val = filter_str.rsplit('=', 1)
+            op = filter_col[-1]
+            filter_col = filter_col[:-1]
+            try:
+                target_param = f[filter_col]
+            except KeyError:
+                if not quiet:
+                    print(
+                        f'  - ✕ ERROR: Filter column `{filter_col}` not found. Filter `{filter_str}` ignored.')
+                continue
+
+            # `filter_col` NOT equal to `filter_val`
+            if op == '!':
+                f = f.loc[target_param != filter_val, :]
+
+                # `filter_col` IS equal to `filter_val`
+            elif op == '=':
+                f = f.loc[target_param == filter_val, :]
+
+            if f.empty:
+                if not quiet:
+                    print(
+                        f'  - Filter expression `{filter_str}` matched zero rows. Filter not applied.')
+                f = input_data.copy()
+            else:
+                filtered = True
+                input_data = f
+                if not quiet:
+                    print(f'  - ✓ Applied filter: `{filter_str}`')
+
+    return input_data, filtered
+
+
+def _select_columns(data_selection: pd.DataFrame,
+                    seek_cols: list,
+                    quiet=False):
+    """
+    Selects columns from a pandas DataFrame based on a list of column names.
+
+    Args:
+        data_selection: The pandas DataFrame to select columns from.
+        seek_cols: A list of column names to select.
+
+    Returns:
+        pd.DataFrame: The pandas DataFrame with only the selected columns.
+
+    Raises:
+        None
+
+    Examples:
+        >>> select_columns(pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]}), ["A"])
+        pd.DataFrame({"A": [1, 2, 3]})
+    """
+
+    if seek_cols:
+        if not quiet:
+            print('\n- *selecting columns...*')
+        selected_cols = []
+        for col in seek_cols:
+            if col not in data_selection.columns and not quiet:
+                print(
+                    f'  - Warning‼️ `{col}` not in columns. Selection ignored.')
+            else:
+                selected_cols.append(col)
+        if selected_cols:
+            data_selection = data_selection[selected_cols]
+    return data_selection
+
+
+def _print_table(data_sample: pd.DataFrame,
+                 max_cols: int = 8,
+                 transpose: bool = False,
+                 markdown: bool = False,
+                 tabbed: bool = False,
+                 comma: bool = False,
+                 piped: bool = False,
+                 quiet: bool = False):
+    if transpose and not quiet:
+        print('_note: table transposed for display_\n')
+        data_sample = data_sample.transpose()
+
+    if piped or markdown or tabbed or comma:
+        data_sample = data_sample.iloc[:, 0:max_cols]
+        if comma:
+            print(data_sample.to_csv(header=True))
+        elif tabbed:
+            print(data_sample.to_csv(header=True, sep='\t'))
+        elif piped:
+            print(data_sample.to_csv(header=True, sep='|'))
+        else:
+            print_md_table(data_sample)
+    else:
+        print('```log')
+        print(data_sample)
+        print('```')
