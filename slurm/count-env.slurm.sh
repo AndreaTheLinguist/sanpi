360a98664ce8f75a736d048ebf1ab223812f181f
Andrea Hummel <andrea.r.hummel@gmail.com>
2024-04-19 23:45:18 -0400

    changed argument parsing:
    count_RBgrams.slurm.sh now uses `getopts`
    ```
    # Parse command-line arguments to customize job parameters
    while getopts ":t:f:d:" opt; do
      case $opt in
        t) THRESH="${OPTARG/\%/}" ;;  # Store the threshold percentage
        f) N_FILES="${OPTARG%f}" ;;  # Store the number of corpus parts
        d) HIT_DATA_DIR="${OPTARG}" ;;
        \?) echo "Invalid option -$OPTARG" >&2; exit 1 ;;
        esac
    done
    ```

--- slurm/count-env.slurm.sh
+++ slurm/count-env.slurm.sh
@@ -21,17 +21,27 @@ echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 eval "$(conda shell.bash hook)"
 conda activate sanpi
 
+# TODO: update this to use `getopts` like `count_RBgrams.slurm.sh`: 
+    # while getopts ":t:f:d:" opt; do
+    #   case $opt in
+    #     t) THRESH="${OPTARG/\%/}" ;;  # Store the threshold percentage
+    #     f) N_FILES="${OPTARG%f}" ;;  # Store the number of corpus parts
+    #     d) HIT_DATA_DIR="${OPTARG}" ;;  
+    #     \?) echo "Invalid option -$OPTARG" >&2; exit 1 ;;
+    #     esac
+    # done
+
 SANPI_DATA="/share/compling/data/sanpi"
 
-ENV_NAME=${1:-'RBdirect'}
+ENV_NAME=${2:-'RBdirect'}
 #> to run on `DEMO` data, prefix argument string with "DEMO/"
-REL_PARENT_DIR=${2:-"2_hit_tables"}
+REL_PARENT_DIR=${3:-"2_hit_tables"}
 HIT_DATA_DIR="${SANPI_DATA}/${REL_PARENT_DIR}/${ENV_NAME}"
 echo "processing ➡️ ${HIT_DATA_DIR//*data/..}"
 
 POST_PROC_DIR="${HIT_DATA_DIR%2_hit_tables*}4_post-processed/RBXadj"
 #> if no argument given, use most recent frequency index .txt file created in nearest 4_post-processed/RBXadj/ dir
-FRQ_FILTER=${3:-"$(ls -t1 $POST_PROC_DIR/*index_frq*.txt  | head -1)"}
+FRQ_FILTER=${1:-"$(ls -t1 $POST_PROC_DIR/*index_frq*.txt  | head -1)"}
 #> if number given instead of path, use value to locate relevant frequency index .txt file
 if [[ ! (-f $FRQ_FILTER && -r $FRQ_FILTER) ]]; then
     FRQ_FILTER="$( (ls -t1 $POST_PROC_DIR/*index_frq*${FRQ_FILTER/./-}p*.txt || ls -t1 $POST_PROC_DIR/*index_frq*txt ) | head -1 )"
--- slurm/count_RBgrams.slurm.sh
+++ slurm/count_RBgrams.slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N1
 #SBATCH -n1
-#SBATCH --mem=50G
+#SBATCH --mem=88G
 #SBATCH -o %x.%j.out
 #SBATCH -e %x.%j.err
 #SBATCH --time 12:00:00
@@ -12,6 +12,7 @@
 # usage:    sbatch [slurm flags] count_RBgrams.slurm.sh [N_FILES] [THRESH] [DATA_DIR_NAME]
 # usage:    sbatch [slurm flags] count_RBgrams.slurm.sh '-f 3' '-t 0.001' '/share/compling/data/sanpi/debug/2_hit_tables/RBXadj'
 SANPI_DATA="/share/compling/data/sanpi"
+RESULTS="/share/compling/projects/sanpi/results"
 
 echo 'running slurm script: /share/compling/projects/sanpi/slurm/count_RBgrams.slurm.sh'
 echo "JOB ID: ${SLURM_JOB_ID}"
@@ -24,29 +25,49 @@ echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 eval "$(conda shell.bash hook)"
 conda activate sanpi
 REL_HIT_DIR="2_hit_tables/RBXadj"
-HIT_DATA_DIR=${1:-"${SANPI_DATA}/${REL_HIT_DIR}"}
-RESULTS="/share/compling/projects/sanpi/results"
+THRESH=''
+HIT_DATA_DIR="${SANPI_DATA}/${REL_HIT_DIR}"
+DIR_N_FILES="$(ls -d1 ${HIT_DATA_DIR}/*pkl.gz | wc -l)"
+N_FILES="${DIR_N_FILES% }"
+# Parse command-line arguments to customize job parameters
+while getopts ":t:f:d:" opt; do
+  case $opt in
+    t) THRESH="${OPTARG/\%/}" ;;  # Store the threshold percentage
+    f) N_FILES="${OPTARG%f}" ;;  # Store the number of corpus parts
+    d) HIT_DATA_DIR="${OPTARG}" ;;  
+    \?) echo "Invalid option -$OPTARG" >&2; exit 1 ;;
+    esac
+done
 
-if [[ $HIT_DATA_DIR == "--demo" ]]; then
-    HIT_DATA_DIR="${SANPI_DATA}/DEMO/${REL_HIT_DIR}"
-    #> replace "results" with "DEMO/results"
-    RESULTS="${RESULTS/results/DEMO/results}"
-fi
 
-mkdir -p $RESULTS
-
-N_FILES=${2:-"$(ls -d1 ${HIT_DATA_DIR}/*pkl.gz | wc -l)"}
-N_FILES="-f ${N_FILES/#* }"
-THRESH=${3:-''}
 if [[ -n $THRESH ]]; then
     THRESH="-t ${THRESH/#* }"
 fi
+
+if [[ $HIT_DATA_DIR == "demo" ]]; then
+    HIT_DATA_DIR="${SANPI_DATA}/DEMO/${REL_HIT_DIR}"
+    #> update number of files in dir
+
+    DIR_N_FILES="$(ls -d1 ${HIT_DATA_DIR}/*pkl.gz | wc -l)"
+    #> replace "results" with "DEMO/results"
+    RESULTS="${RESULTS/results/'DEMO/results'}"
+fi
+
+N_FILES=${N_FILES// /}
+if (( ${N_FILES} > ${DIR_N_FILES% } )); then
+    N_FILES=${DIR_N_FILES// /}
+fi
+
+N_FILES=${N_FILES:+"-f ${N_FILES}"}
+
+mkdir -p $RESULTS
 
 DATA_DIR_NAME=$(basename ${HIT_DATA_DIR})
 echo "processing ➡️ ${HIT_DATA_DIR//*data/..}"
 POST_PROC_DIR="${HIT_DATA_DIR%2_hit_tables*}4_post-processed/${DATA_DIR_NAME}"
 PROG="/share/compling/projects/sanpi/source/analyze/count_bigrams.py -p ${POST_PROC_DIR} -o ${RESULTS}/freq_out/${DATA_DIR_NAME}"
-
-echo 'time python $PROG $N_FILES $THRESH -d $HIT_DATA_DIR'
+# usage: count_bigrams.py [-h] [-f N_FILES] [-t PERCENT_HITS_THRESHOLD] [-d DATA_DIR] [-p POST_PROC_DIR] [-o FRQ_OUT_DIR] [-g FRQ_GROUPS] [-s]
+# echo 'time python $PROG $N_FILES $THRESH -d $HIT_DATA_DIR'
 echo "time python $PROG $N_FILES $THRESH -d $HIT_DATA_DIR"
-time python $PROG $N_FILES $THRESH -d $HIT_DATA_DIR #>> >(tee -i -a ${LOG_FILE}) 2>&1
+time python $PROG $N_FILES $THRESH -d $HIT_DATA_DIR 
+#// >> >(tee -i -a ${LOG_FILE}) 2>&1
