ea19883490f29f964a98a5e97bbcb6ce5f09e644
Andrea Hummel <andrea.r.hummel@gmail.com>
2024-07-16 18:52:51 -0400

    renamed `get_pre-cleaned_subtables.py` ➡️ `clean_bigrams_by_part.py`

--- slurm/com-process_by-part.slurm.sh
+++ slurm/com-process_by-part.slurm.sh
@@ -19,8 +19,8 @@ echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 eval "$(conda shell.bash hook)"
 conda activate sanpi
 LOG_FILE=${SLURM_JOB_NAME}.JID${SLURM_JOB_ID}_$(date +%y-%m-%d_%H%M).log
-echo "time python "/share/compling/projects/sanpi/script/get_pre-cleaned_subtables.py" >> >(tee -i -a ${LOG_FILE}) 2>&1"
-time python "/share/compling/projects/sanpi/script/get_pre-cleaned_subtables.py" >> >(tee -i -a ${LOG_FILE}) 2>&1
+echo "time python "/share/compling/projects/sanpi/script/clean_bigrams_by_part.py" >> >(tee -i -a ${LOG_FILE}) 2>&1"
+time python "/share/compling/projects/sanpi/script/clean_bigrams_by_part.py" >> >(tee -i -a ${LOG_FILE}) 2>&1
 echo "Finished."
 date
 exit
